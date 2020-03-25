06ee87d15d8599e09ae2cb38d3facb55f84d6311
Yevhen <elaichenkov@gmail.com>
2020-03-25 20:07:01 +0200

    add(exercises): complete all tasks

--- Exercises/1-hoisting.js
+++ Exercises/1-hoisting.js
@@ -1,5 +1,11 @@
 'use strict';
 
-const fn = null;
+const fn = () => {
+  // eslint-disable-next-line no-use-before-define
+  console.log(value);
+  // eslint-disable-next-line no-var
+  var value = 4;
+
+};
 
 module.exports = { fn };
--- Exercises/4-count-types.js
+++ Exercises/4-count-types.js
@@ -1,5 +1,16 @@
 'use strict';
 
-const countTypesInArray = null;
+const countTypesInArray = arr => {
+  const result = {};
+
+  for (const item of arr) {
+    const type = typeof item;
+    const count = result[type] || 0;
+
+    result[type] = count + 1;
+  }
+
+  return result;
+};
 
 module.exports = { countTypesInArray };
